ddef6e4f3e2e66dd68f83069b16cc8b5f9330204
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-04-10 20:54:03 -0700

    Revert "finished 5.3"
    This reverts commit 59d2fb8e40a423a9d5b1e7c462a6aac2727c4bdb.

--- Assets/Course Library/Scripts/GameManager.cs
+++ Assets/Course Library/Scripts/GameManager.cs
@@ -2,28 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI gameOverText;
-    public Button restartButton;
-    public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        score = 0;
-
         StartCoroutine(SpawnTarget());
+        score = 0;
         UpdateScore(0);
-
     }
 
     // Update is called once per frame
@@ -34,7 +26,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while(isGameActive)
+        while(true)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -46,17 +38,5 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score " + score;
-    }
-
-    public void GameOver()
-    {
-        restartButton.gameObject.SetActive(true);
-        gameOverText.gameObject.SetActive(true);
-        isGameActive = false;
-    }
-
-    public void RestartGame()
-    { 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
